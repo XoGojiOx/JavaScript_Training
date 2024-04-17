889ffc52adc0fd9ab6f69173883af22d761699a4
Richard Sanders <richcodes18@gmail.com>
2024-04-17 09:45:38 -0500

    Ready

--- script.js
+++ script.js
@@ -4,15 +4,71 @@
 10 and above $20
 */
 
-let infant = "$0";
-let child = "$10";
-let adult = "$20";
-let age = prompt("What is your age? ");
+// let infant = "$0";
+// let child = "$10";
+// let adult = "$20";
+// let age = prompt("What is your age? ");
 
-if (age <= 5) {
-  console.log(`You get in for ${infant}`);
-} else if (age <= 10) {
-  console.log(`You need to pay ${child}`);
-} else {
-  console.log(`You need to pay ${adult}`);
-}
+// if (age <= 5) {
+//   console.log(`You get in for ${infant}`);
+// } else if (age <= 10) {
+//   console.log(`You need to pay ${child}`);
+// } else {
+//   console.log(`You need to pay ${adult}`);
+// }
+
+// let password = prompt("Enter your password here: ");
+
+// if (password.length >= 6) {
+//   if (password.indexOf(" ") === -1) {
+//     console.log("That works Great!");
+//   } else {
+//     console.log("Password cannot contain any spaces");
+//   }
+// } else {
+//   console.log("To short! Password must be 6+ characters long");
+// }
+
+// if (password.indexOf(" ") === -1) {
+//   console.log("That Works!");
+// } else {
+//   console.log("Please do not include spaces");
+// }
+
+// let input = prompt("Enter You Password");
+
+// if (input.length >= 6 && input.indexOf(" ") === -1) {
+//   console.log("New Password Has Been Set");
+// } else {
+//   console.log("Make sure Password is 6+ characters with no spaces");
+// }
+
+// const day = 8;
+
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("There is no day of the week with that number");
+// }
+
+// INTRODUCTION TO ARRAYS
