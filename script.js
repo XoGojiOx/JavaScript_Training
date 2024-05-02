97cc76eba489324d855cc5eb44659275ee3bb335
Richard Sanders <xogojiox@gmail.com>
2024-05-02 10:14:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -556,3 +556,71 @@ It should return the sum of all the numbers in the array.
 //   }
 //   return sum;
 // }
+
+/* Days of the week exercise
+Write a function called returnDay. this function takes in one parameter (a number from 1-7) and returns the day of the week (1 is Monday, 2 is Tuesday, etc.)  If the number is less than 1 or greater than 7, the function should return null. In some countries Sunday is treated as the first day of the week, but for this exercise we will use Monday as the first day.
+
+Hint: store the days of the week in an array, or use an object with numerical keys.  When the function is called, plug the number into the array/object you've created to retrieve the corresponding day name and then return that value.
+
+returnDay(1) // "Monday"
+returnDay(7) // "Sunday"
+returnDay(4) // "Thursday"
+returnDay(0) // null
+
+CODE
+function returnDay(num) {
+    const days = {
+        1: 'Monday',
+        2: 'Tuesday',
+        3: 'Wednesday',
+        4: 'Thursday',
+        5: 'Friday',
+        6: 'Saturday',
+        7: 'Sunday'
+    }
+    if(num < 1 || num > 7) {
+        return null;
+    } else {
+        return days[num];
+    }
+}
+*/
+
+// Scopes  > Function Scopre > Block Scope > Lexical Scope
+// Scope is a variable's visibility
+//Example
+// function collectEggs(){
+//   let totalEggs = 6;
+//   return totalEggs;
+// }
+// ^^^ I can only call totalEggs within the function.
+// If I call totalEggs below the function it will not run
+
+// Block Scope
+// let radius = 8;
+// if (radius > 0) {
+//   const PI = 3.14159;
+//   let msg = "Hi";
+// }
+// { Everything inside these brackets are BLOCKED scoped }
+
+/* Function Expressions */
+// const square = function (num) {
+//   return num * num;
+// };
+// square(7); // returns 49
+/* The function does not have a name, just (). The name is the variable name */
+
+/* Passing a function within a function */
+function rollDie() {
+  let roll = Math.floor(Math.random() * 6) + 1;
+  console.log(roll);
+}
+
+function timesTen(t) {
+  for (let i = 0; i < 10; i++) {
+    t();
+  }
+}
+
+timesTen(rollDie);
